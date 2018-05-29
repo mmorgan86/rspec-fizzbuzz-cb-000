2457e51b66032feb7d996edaf81104683a6d9890
Michael Morgan <michael1986morgan@gmail.com>
2018-05-29 23:24:46 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(num)
-  if num % 3 == 0
+  if (num % 5 == 0) && (num % 3 == 0)
+    "FizzBuzz"
+  elsif num %  == 0
     "Fizz"
   elsif num % 5 == 0
     "Buzz"
-  elsif (num % 5 == 0) && (num % 3 == 0)
-    "FizzBuzz"
   else
-    "nil"
+    nil
   end
 end
