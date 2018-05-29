8c5bb2f13d1fccc056aadace63712468d90c1331
Michael Morgan <michael1986morgan@gmail.com>
2018-05-29 23:21:36 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,8 @@
 def fizzbuzz(num)
   if num % 3 == 0
-    puts "Fizz"
+    "Fizz"
   elsif num % 5 == 0
-    num = "Buzz"
-    puts num
+    "Buzz"
   elsif (num % 5 == 0) && (num % 3 == 0)
     num = "FizzBuzz"
     puts num
